986a3ac69224768676c0c0edc502673e0b3a0edf
Nyururukelvin <nyururukelvin9@gmail.com>
2020-11-10 22:34:54 +0300

    my commit add files

--- FastFoodFast/settings.py
+++ FastFoodFast/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Food.apps.FoodConfig',
+    'authentication.apps.AuthenticationConfig'
 ]
 
 MIDDLEWARE = [
@@ -121,3 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+}
---
+++ Food/api.py
@@ -0,0 +1,10 @@
+from rest_framework.response import Response
+from rest_framework import generics,permissions
+
+from .serializers import CustomerSerializer
+from .models import Customer
+
+class CustomerApi(generics.ListCreateAPIView):
+    
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
