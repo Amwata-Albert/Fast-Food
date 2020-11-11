b2ab9acf5146e0f09f3377b151e6b06f010d0e0a
Amwata Albert <albertotieno41@mail.com>
2020-11-11 21:47:51 +0300

    my commit add rest-framework

--- Food/urls.py
+++ Food/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from .api import MealsApi
 
+from django.urls import include, path
+from rest_framework import routers
+from . import views
 
 urlpatterns = [
     path('api/Meals',MealsApi.as_view())
--- Food/views.py
+++ Food/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework import viewsets,routers
+
+from .serializers import Mealserializer
+from .models import Meals
+from . import views
 
 # Create your views here.
+class MealViewSet(viewsets.ModelViewSet):
+    queryset = Meals.objects.all().order_by('name')
+    serializer_class = Mealserializer
+
+
+router = routers.DefaultRouter()
+router.register(r'meals', views.MealViewSet)
