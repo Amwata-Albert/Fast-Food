525f1cb0513d4ee195dda7a6ee3542e71dfbb666
Nyururukelvin <nyururukelvin9@gmail.com>
2020-11-12 14:30:40 +0300

    my commit make changes

--- FastFoodFast/settings.py
+++ FastFoodFast/settings.py
@@ -38,11 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Food.apps.FoodConfig',
-    'authentication.apps.AuthenticationConfig',
+    
     'rest_framework',
 ]
 
-AUTH_USER_MODEL = 'authentication.User'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
