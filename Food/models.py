8a37a10379be6d97f54089feb3c1d271306c9075
Nyururukelvin <nyururukelvin9@gmail.com>
2020-11-19 12:25:30 +0300

    my commit edit model

--- Food/models.py
+++ Food/models.py
@@ -19,7 +19,7 @@ class Meals(models.Model):
 	category=models.CharField(max_length=20,null=True)
 
 	def __str__(self):
-		return self.meals
+		return self.name
 
 class CurrentOrders(models.Model):
 	food=models.ForeignKey(Meals,null=True,on_delete=models.PROTECT)
