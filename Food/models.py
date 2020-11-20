9e71a8e1833c88761434ff9a1e458a290d0bea4f
Nyururukelvin <nyururukelvin9@gmail.com>
2020-11-20 09:43:16 +0300

    my commit add cloudinary field

--- Food/models.py
+++ Food/models.py
@@ -1,7 +1,4 @@
 from django.db import models
-import cloudinary
-import cloudinary.uploader
-import cloudinary.api
 from cloudinary.models import CloudinaryField
 
 # Models
@@ -19,7 +16,7 @@ class Meals(models.Model):
 	food_id=models.IntegerField(primary_key=True)
 	name=models.CharField(max_length=50,null=True)
 	price=models.IntegerField(null=True)
-	photo=models.ImageField(upload_to='meals/', default='No image')
+	photo=CloudinaryField('image')
 	category=models.CharField(max_length=20,null=True)
 
 	def __str__(self):
