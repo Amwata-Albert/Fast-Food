11166724ca912f81ce1be0166138355c85d3d513
Nyururukelvin <nyururukelvin9@gmail.com>
2020-11-20 12:04:02 +0300

    my commit add api end points

--- Food/api.py
+++ Food/api.py
@@ -1,9 +1,25 @@
 from rest_framework.response import Response
 from rest_framework import generics,permissions
-from .serializers import Mealserializer,CustomerSerializer
-from .models import Meals,Customer
+from .serializers import Mealserializer,CustomerSerializer,CurrentOrdersSerializer,OrderHistorySerializer
+from .models import Meals,Customer,CurrentOrders,OrderHistory
+
+
+class OrderHistoryApi(generics.ListCreateAPIView):
+
+    queryset=OrderHistory.objects.all()
+    serializer_class=OrderHistorySerializer
+
+class OrderHistoryDetailApi(generics.RetrieveUpdateDestroyAPIView):
+
+    queryset=OrderHistory.objects.all()
+    serializer_class=OrderHistorySerializer
 
 class mealsApi(generics.ListCreateAPIView):
+
+    queryset=Meals.objects.all()
+    serializer_class=Mealserializer
+
+class mealsDetailApi(generics.RetrieveUpdateDestroyAPIView):
 
     queryset=Meals.objects.all()
     serializer_class=Mealserializer
@@ -16,4 +32,15 @@ class CustomerApi(generics.ListCreateAPIView):
 class CustomerDetailApi(generics.RetrieveUpdateDestroyAPIView):
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
+
+class CurrentOrdersApi(generics.ListCreateAPIView):
+    
+    queryset = CurrentOrders.objects.all()
+    serializer_class = CurrentOrdersSerializer
+
+class CurrentOrderDetailApi(generics.RetrieveUpdateDestroyAPIView):
+    queryset = CurrentOrders.objects.all()
+    serializer_class = CurrentOrdersSerializer
+
+
 
--- Food/urls.py
+++ Food/urls.py
@@ -1,11 +1,16 @@
-from .api import mealsApi,CustomerApi,CustomerDetailApi
+from .api import mealsApi,CustomerApi,CustomerDetailApi,CurrentOrdersApi,CurrentOrderDetailApi,mealsDetailApi,OrderHistoryApi,OrderHistoryDetailApi
 from django.urls import include, path
 from rest_framework import routers
 from . import views
 
 urlpatterns = [
     path('api/meals',mealsApi.as_view()),
+    path('api/meals/<int:pk>',mealsDetailApi.as_view()),
+    path('api/orderhistory',OrderHistoryApi.as_view()),
+    path('api/orderhistory/<int:pk>',OrderHistoryDetailApi.as_view()),
     path('api/customer', CustomerApi.as_view()),
     path('api/customer/<int:pk>', CustomerDetailApi.as_view()),
+    path('api/currentorder', CurrentOrdersApi.as_view()),
+    path('api/currentorder/<int:pk>', CurrentOrderDetailApi.as_view()),
 ]
 
